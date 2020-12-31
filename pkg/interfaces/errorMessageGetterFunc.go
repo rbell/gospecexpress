7fec0af5eb3a557137c141f3ed6d158b6c11a826
rabell <rabell@fanatics.com>
2020-12-31 09:05:18 -0700

    licensing

--- pkg/interfaces/errorMessageGetterFunc.go
+++ pkg/interfaces/errorMessageGetterFunc.go
@@ -1,3 +1,7 @@
+// Copyright ©2021 by Randy R Bell. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package interfaces
 
 // ErrorMessageGetterFunc defines a function that, given error message context, return an error string
