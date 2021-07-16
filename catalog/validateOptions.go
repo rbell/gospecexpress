003b15e427edccd14f962d7e754a85946f33760c
rabell <rabell@fanatics.com>
2021-07-16 10:10:09 -0600

    anotherfix

--- catalog/validateOptions.go
+++ catalog/validateOptions.go
@@ -1,22 +1,10 @@
 package catalog
 
-import "reflect"
-
-// WithContext adds contextItems to the context, keyed by each item's type name.
-// Perfect when passing a list of items where each item is of different type.
-func WithContext(contextItems ...interface{}) func(something interface{}, context map[string]interface{}) {
+// WithContext adds contextItems to the context
+func WithContext(contextItems map[string]interface{}) func(something interface{}, context map[string]interface{}) {
 	return func(something interface{}, context map[string]interface{}) {
-		for _, item := range contextItems {
-			t := reflect.TypeOf(item)
-			key := t.String()
-			if t.Kind() == reflect.Ptr {
-				if t.Elem().Kind() == reflect.Interface {
-					key = t.Elem().Name()
-				} else {
-					key = t.Elem().String()
-				}
-			}
-			context[key] = item
+		for k, item := range contextItems {
+			context[k] = item
 		}
 	}
 }
