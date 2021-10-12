8decdf3ae45ba3302d946c11ab5a2fcc2493a985
rabell <rabell@fanatics.com>
2021-10-12 09:54:38 -0600

    Fix to error factory func

--- errors/validatorError.go
+++ errors/validatorError.go
@@ -37,7 +37,7 @@ func NewValidationErrors(errs map[string][]string, children map[string]*Validato
 
 // NewValidationErrorsWithWarnings returns a new validation error for a map of error messages
 func NewValidationErrorsWithWarnings(errs, warnings map[string][]string, children map[string]*ValidatorError) *ValidatorError {
-	if errs == nil {
+	if errs == nil && warnings == nil {
 		return &ValidatorError{errorMap: make(map[string][]string), children: children}
 	}
 	return &ValidatorError{errorMap: errs, warningMap: warnings, children: children}
